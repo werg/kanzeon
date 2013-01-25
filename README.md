91e8a95588d2f1c4ff1d1724d285a2cafc5bae7d
Gabriel Pickard <werg@tick.(none)>
2013-01-25 00:53:54 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# kanzeon
+
+A malleable logic programming language for Clojure
+
+
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 FIXME
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject kanzeon "0.1.0-SNAPSHOT"
+  :description "A malleable logic programming language"
+  :url "http://github.com/werg/kanzeon"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-expectations "0.0.7"]])
